1329c9b705f307747d0fc30988c4818a7b0861ae
Brett Adamson <adamsonb12@gmail.com>
2024-11-03 09:25:28 -0700

    first commit

---
+++ src/stories/clock/Clock.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Clock } from './Clock';
+
+const meta = {
+  title: 'Clock',
+  component: Clock,
+} satisfies Meta<typeof Clock>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
